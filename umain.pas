m-audio91/FaSubrip

commit 4fe45b71f8743c999c254ac339c4bdefd02a8bc7

Fixed regression after commit 1b5cd6d
If both Source and Destination is Unicode there is no reason to remove unicode direction control characters. unless the user wants to (so additional options where added to OutFileEncoding combobox instead)

umain.pas
@@ -157,13 +157,14 @@ const
   MoreBadPhrasesEndMarker = '[endofmorebadphrases]';
   ArabicChars: array[0..1] of String = ('ك', 'ي');
   FarsiChars: array[0..1] of String = ('ک', 'ی');
-  EncodingNames: array[0..2] of String = ('UTF-8', 'WINDOWS-1256', 'UTF-16');
+  EncodingNames: array[0..4] of String = ('UTF-8', 'UTF-8noRTL',
+    'WINDOWS-1256', 'UTF-16', 'UTF-16noRTL');
   UnicodeBOMs: array[0..4] of String = (UTF8BOM, UTF16BEBOM, UTF16LEBOM, UTF32BEBOM,
     UTF32LEBOM);
-  UnicodeDirectionControllers: array[0..11] of String = (#$E2#$80#$8F
-  ,#$D8#$9C, #$E2#$80#$8E, #$E2#$80#$AA, #$E2#$80#$AB, #$E2#$80#$AD
-  ,#$E2#$80#$AC, #$E2#$80#$AE, #$E2#$81#$A6, #$E2#$81#$A7, #$E2#$81#$A8
-  ,#$E2#$81#$A9);
+  UnicodeDirectionControllers: array[0..11] of String = (#$E2#$80#$8F,
+    #$D8#$9C, #$E2#$80#$8E, #$E2#$80#$AA, #$E2#$80#$AB, #$E2#$80#$AD,
+    #$E2#$80#$AC, #$E2#$80#$AE, #$E2#$81#$A6, #$E2#$81#$A7, #$E2#$81#$A8,
+    #$E2#$81#$A9);
   CommonPunctuations: array[0..9] of String = ('!',',','.',':',';',
     '?','‚','؟','؛','،');
 
@@ -406,8 +407,11 @@ var
 begin
   for c in UnicodeBOMs do
     DeleteAllOccurrences(c, S);
-  for c in UnicodeDirectionControllers do
-    DeleteAllOccurrences(c, S);
+  case OutFileEncoding.ItemIndex of
+  1,2,4:
+    for c in UnicodeDirectionControllers do
+      DeleteAllOccurrences(c, S);
+  end;
 end;
 
 procedure TFaSubripMain.SwapArabicChars(var S: String);
@@ -575,32 +579,34 @@ var
   sl: TStringList;
   Enc: TEncoding;
   Sub: String;
+  OutEnc: Integer;
 begin
   Sub := Subtitle;
+  OutEnc := OutFileEncoding.ItemIndex;
   if FInputFile.Equals(Sub) and FileExists(Sub) then
     DeleteFile(Sub);
   if AppendEncodingToFileName.State = cbChecked then
   begin
-    if not Sub.EndsWith(EncodingNames[OutFileEncoding.ItemIndex]+extSrt) then
-      Sub := GenFileName(Sub, '_'+EncodingNames[OutFileEncoding.ItemIndex]);
+    if not Sub.EndsWith(EncodingNames[OutEnc]+extSrt) then
+      Sub := GenFileName(Sub, '_'+EncodingNames[OutEnc]);
   end;
   Enc := Default(TEncoding);
   sl := TStringList.Create;
   try
-    case OutFileEncoding.ItemIndex of
-    0:begin
+    case OutEnc of
+    0..1:begin
       sl.DefaultEncoding := Enc.UTF8;
       sl.Text := FSrt;
       sl.TextLineBreakStyle := DefaultTextLineBreakStyle;
       sl.SaveToFile(Sub, Enc.UTF8);
       end;
-    1:begin
+    2:begin
       sl.DefaultEncoding := Enc.ANSI;
       sl.Text := ConvertEncoding(FSrt, EncodingUTF8, EncodingCP1256);
       sl.TextLineBreakStyle := DefaultTextLineBreakStyle;
       sl.SaveToFile(Sub);
       end;
-    2:begin
+    3..4:begin
       sl.DefaultEncoding := Enc.Unicode;
       sl.Text := FSrt;
       sl.TextLineBreakStyle := DefaultTextLineBreakStyle;

umain.lfm
@@ -15,6 +15,7 @@ object FaSubripMain: TFaSubripMain
   OnShow = FormShow
   ParentFont = True
   SessionProperties = 'AppendEncodingToFileName.State;ArabicCharsToFarsi.State;CensorshipLevel.ItemIndex;EndingPunctuations.ItemIndex;OpenDlg.InitialDir;OutFileEncoding.ItemIndex;PhrasesCensorshipFile.Text;PhrasesCensorship.State;ReplaceSourceFile.State;StripHTMLFontTags.State;StripHTMLStyleTags.State;Left;Top'
+  LCLVersion = '2.1.0.0'
   object Header: TPanel
     Left = 0
     Height = 30
@@ -312,7 +313,7 @@ object FaSubripMain: TFaSubripMain
         object EndingPunctuationsL: TLabel
           Left = 202
           Height = 28
-          Hint = 'یکی از مشکلات شایع برخی نرم افزارها با زبان فارسی مشکل نمایش'#10'کامل راست به چپ هست که حتی اگر متن به درستی نمایش داده شود'#10'باز علایمی مانند نقطه و علامت سوال و غیره که در انتهای خط تایپ'#10'شده اند در هنگام نمایش در ابتدای خط نمایش داده می شوند.'#10'کاری که این گزینه می کند این است که فقط علایم انتهای خطوط را به'#10'ابتدای خط منتقل می کند یا بلعکس تا شاید در زمان نمایش در'#10'پخش کننده های مذکور در جایی که باید نمایش داده بشوند.'#10'همچنین امکان حذف جدا یا همه علایم ابتدایی و انتهایی نیز گنجانده شده.'#10'تذکر: '#10'این عملیات قابلیت برگشت ندارد لذا درصورت لزوم یک نسخه کپی'#10'از زیرنویس اصلی نگه دارید.'
+          Hint = 'یکی از مشکلات شایع برخی نرم افزارها با زبان فارسی مشکل نمایش'#10'کامل راست به چپ هست که حتی اگر متن به درستی نمایش داده شود'#10'باز علایمی مانند نقطه و علامت سوال و غیره که در انتهای خط تایپ'#10'شده اند در هنگام نمایش در ابتدای خط نمایش داده می شوند.'#10'کاری که این گزینه می کند این است که فقط علایم انتهای خطوط را به'#10'ابتدای خط منتقل می کند یا بلعکس تا شاید در زمان نمایش در'#10'پخش کننده های مذکور در جایی که باید نمایش داده بشوند.'#10'همچنین امکان حذف جدا یا همه علایم ابتدایی و انتهایی نیز گنجانده شده.'#10'تذکر: '#10'این عملیات قابلیت برگشت ندارد لذا درصورت لزوم یک نسخه کپی'#10'از زیرنویس اصلی نگه دارید.'#10'این گزینه عمدتا برای خروجی های ویندوز-۱۲۵۶ و دو یونیکدی'#10'که در نامشان no Right-To-Left Mark دارند قرار داده شده.'
           Top = 0
           Width = 225
           Caption = 'علایم نگارشی ابتدا و انتهای سطور را'
@@ -500,8 +501,10 @@ object FaSubripMain: TFaSubripMain
           ItemIndex = 0
           Items.Strings = (
             'UTF-8'
+            'UTF-8 - no Right-To-Left mark'
             'ANSI WINDOWS-1256'
             'Unicode (UTF-16 LE)'
+            'Unicode (UTF-16 LE) - no Right-To-Left mark'
           )
           ParentBidiMode = False
           ParentColor = True

commit 9a6f345b3a3df6752f536448b536ba578c46ca00

fixed regression. SwapArabicChars should automatically be uncheched when output encodings is ANSI
regression after commit  4fe45b7

umain.pas
@@ -416,7 +416,7 @@ end;
 
 procedure TFaSubripMain.SwapArabicChars(var S: String);
 begin
-  if OutFileEncoding.ItemIndex = 1 then
+  if OutFileEncoding.ItemIndex = 2 then
     ArabicCharsToFarsi.State := cbUnchecked;
   if ArabicCharsToFarsi.State <> cbChecked then
     S := ReplaceStrings(S, FarsiChars, ArabicChars)